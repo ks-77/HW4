3a5472836d10e48d21789a903990117eb7d20990
ks-77 <deka0987654@gmail.com>
2024-01-11 13:26:01 +0200

    first-task completed

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 HW4
 Savchenko Kirill
 """
+
+# 1
+sentence = input("Enter any sentence with letters and numbers: ")
+
+letter_q = 0
+number_q = 0
+
+for symbol in sentence:
+    if symbol.isalpha():
+        letter_q += 1
+    elif symbol.isdigit():
+        number_q += 1
+print(f"letters in text: {letter_q}")
+print(f"numbers in text: {number_q}")
