e502ed627ace327bb503b119530d8089a15881ff
ks-77 <deka0987654@gmail.com>
2024-01-12 02:12:50 +0200

    additional-task completed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 HW4
 Savchenko Kirill
 """
+import re
+import string
 
 # 1
 sentence = input("Enter any sentence with letters and numbers: ")
@@ -47,3 +49,33 @@ print(text[2:: 2])
 print(text[:: -1])
 print(text[::-2])
 print(len(text))
+
+# additional task
+
+text = """martha sees bananas are on sale! bananas are usually 3 dollars per bunch, but they are 1 dollar today.
+ martha wants 3 bunches of bananas because she is going to make banana bread. this would normally cost Martha 9 dollars,
+ but today it costs Martha 3 dollars. martha saved 6 dollars!"""
+
+sentences = re.split(r'(?<=[.!?])\s+', text)
+
+for sentence in sentences:
+    print(sentence.capitalize())
+
+"""
+capitalized_sentences = [sentence.capitalize() for sentence in sentences]
+result_text = ' '.join(capitalized_sentences)
+
+print(result_text)
+"""
+
+print(f"number of \"!\": {text.count("!")}")
+
+punctuation_count = sum(1 for char in text if char in string.punctuation)
+print(f"number of punctuation mark: {punctuation_count}")
+
+number_q = 0
+
+for symbol in text:
+    if symbol.isdigit():
+        number_q += 1
+print(number_q)
